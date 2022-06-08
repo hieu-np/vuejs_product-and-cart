17b56e842b73d5f0974b43c7989e1d593ab17846
nguyenhieu409 <hieu.nguyen@successsoftware.global>
2022-06-08 15:52:17 +0700

    update

---
+++ src/views/Products.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <ProductCart
+        v-for="(product, index) in inventory"
+        :key="product.id"
+        :index="index"
+        :product="product"
+        :addToCart="addToCart"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCart from '@/components/ProductCart.vue'
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCart
+  }
+}
+</script>
